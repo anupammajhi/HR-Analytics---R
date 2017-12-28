e9d3ce9489e5a995fa01c8e72cd09d36963d5960
Anupam Majhi <majhi.anupam@gmail.com>
2017-12-28 08:05:00 +0530

    Line update

---
+++ main.R
@@ -0,0 +1,32 @@
+
+# Clear All Variables
+rm(list=ls())
+
+#================= INITIAL STEPS ====================
+
+# Import Libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(corrplot)
+library(MASS)
+library(car)
+library(e1071)
+library(caret)
+library(caTools)
+library(ROCR)
+library(DAAG)
+
+
+# Import Data Files
+emp_survey <- read.csv("employee_survey_data.csv", stringsAsFactors = T)
+gen_data <- read.csv("general_data.csv",stringsAsFactors = T)
+in_time <- read.csv("in_time.csv",stringsAsFactors = T)
+out_time <- read.csv("out_time.csv",stringsAsFactors = T)
+manager_survey <- read.csv("manager_survey_data.csv",stringsAsFactors = T)
+
+# Function to replace NA by mean
+replace_NA_by_mean <- function(DFcolumn){
+  DFcolumn[is.na(DFcolumn)] <- round(mean(DFcolumn[!is.na(DFcolumn)]))
+  DFcolumn
+}
